bbb5348124b8bb022124752eb50aeb1cf820a0af
Kavosh Asadi <k8@brown.edu>
2018-03-06 14:23:10 -0500

    cleaned up code, specifically transition model is now in a separate class

--- deep/main.py
+++ deep/main.py
@@ -2,17 +2,8 @@ import numpy
 import numpy.linalg as lg
 import matplotlib.pyplot as plt
 import sys
-import net
+import transition_model
 
-def linear_regression(phi,y,w):
-	first=lg.inv(numpy.matmul(numpy.matmul(numpy.transpose(phi),w),phi))
-	second=numpy.matmul(numpy.matmul(numpy.transpose(phi),w),y)
-	theta=first * second
-	return theta
-def deep_regression(phi,y,net,w):
-	net.fit(x=phi,y=y,epochs=100, verbose=1,sample_weight=w)
-	return net
-	#sys.exit(1)
 def create_train_data(num_samples,num_lines):
 	li_samples=[]
 	li_labels=[]
@@ -23,6 +14,8 @@ def create_train_data(num_samples,num_lines):
 				label=numpy.sin(3*sample)+numpy.random.normal(loc=0.0, scale=0.0,size=1)
 			elif l==1:
 				label=numpy.cos(3*sample)+numpy.random.normal(loc=0.0, scale=0.0,size=1)
+			elif l==2:
+				label=numpy.sin(3*sample)*numpy.cos(3*sample)+numpy.random.normal(loc=0.0, scale=0.0,size=1)
 			else:
 				print('not implemented yet ... aborting')
 				sys.exit(1)
@@ -40,77 +33,63 @@ def create_matrices(li_samples,li_labels):
 	mat_labels=numpy.matrix(arr_labels)
 	return mat_samples,mat_labels
 
-def compute_posterior(net1,net2,phi,y):
-	out1=net1.predict(phi)
-	out2=net2.predict(phi)
+def compute_posterior(tm,phi,y):
 
-	prob1=numpy.array(y-out1)
-	prob1=numpy.exp(-numpy.multiply(prob1,prob1)/(.05))
-	prob1_sum=numpy.multiply(prob1,numpy.log(prob1))
+	o_li=[x.predict(phi) for x in tm.models]
+	p_li=[]
+	for o in o_li:
+		p=numpy.array(y-o)
+		p=numpy.exp(-numpy.multiply(p,p)/(.05)).flatten()
+		p_li.append(p)
+	p_arr=numpy.transpose(numpy.array(p_li))
+	for i in range(p_arr.shape[0]):
+		sum_probs=numpy.sum(p_arr[i,:])
+		for j in range(p_arr.shape[1]):
+			p_arr[i,j]=p_arr[i,j]/sum_probs
+	p_li=[]
+	p_arr=numpy.transpose(p_arr)
+	for i in range(len(p_arr)):
+		p_li.append(p_arr[i,:])
+	return p_li
 
 
-	prob2=numpy.array(y-out2)
-	prob2=numpy.exp(-numpy.multiply(prob2,prob2)/(.05))
-	prob2_sum=numpy.multiply(prob2,numpy.log(prob2))
-	obj=numpy.sum(prob1_sum)+numpy.sum(prob2_sum)
-	#print(prob2)
-	#sys.exit(1)
-	normalized_prob1=[]
-	normalized_prob2=[]
-	for index,(p1,p2) in enumerate(zip(prob1,prob2)):
-		#print(p1[0],p2[0])
-		normalized_prob1.append(p1[0]/(p1[0]+p2[0]))
-		normalized_prob2.append(p2[0]/(p1[0]+p2[0]))
-		#print(normalized_prob1,normalized_prob2)
-		#sys.exit(1)
-	normalized_prob1=numpy.array(normalized_prob1)
-	normalized_prob2=numpy.array(normalized_prob2)
-	return normalized_prob1,normalized_prob2,obj
-def compute_em_objective():
-	print("to be implemented")
+def plot_everything(li_samples,li_labels,tm,phi,ax):
+	ax.clear()
+	ax.plot(li_samples,li_labels,'o')
+	y_li=tm.predict(phi)
+	for y in y_li:
+		ax.plot(li_samples,y,'o',lw=2)
+
 
 num_experiments=1
-num_samples=20
-num_lines=2
+num_samples=100
+num_lines=3
 num_iterations=100
+plot=True
+
+model_params={}
+model_params['lipschitz_constant']=0.6
+model_params['num_hidden_layers']=1
+model_params['hidden_layer_nodes']=64
+model_params['activation_fn']='relu'
+model_params['learning_rate']=0.001
+model_params['observation_size']=1
+model_params['num_models']=3
+model_params['num_epochs']=100
+
+
 fig = plt.figure()
-#plt.pause(10)
-ax1 = fig.add_subplot(1,1,1)
+ax = fig.add_subplot(1,1,1)
 li_obj_all=[]
 for experiment in range(num_experiments):
 	li_samples,li_labels=create_train_data(num_samples,num_lines)
 	phi,y=create_matrices(li_samples,li_labels)
-	net1=net.create_model()
-	net2=net.create_model()
-	li_obj=[]
-	plot=True
-	#plt.pause(10)
+	tm=transition_model.neural_transition_model(model_params)
 	for iteration in range(num_iterations):
-		#obj=compute_em_objective()
-		#sys.exit(1)
-		w1,w2,obj=compute_posterior(net1,net2,phi,y)
-		li_obj.append(obj)
+		w_li=compute_posterior(tm,phi,y)#E step
 		if iteration>0:
-			net1=deep_regression(phi,y,net1,w1)
-			net2=deep_regression(phi,y,net2,w2)
-		y1=net1.predict(phi)
-		y2=net2.predict(phi)
-		#print(y1)
-		#sys.exit(1)
+			tm.regression(phi,y,w_li)#M step
 		if plot==True:
-			ax1.clear()
-			ax1.plot(li_samples,li_labels,'o')
-			ax1.plot(li_samples,y1,'o',lw=2)
-			ax1.plot(li_samples,y2,'o',lw=2)
-
-			plt.pause(.05)
-		#sys.exit(1)
-	li_obj_all.append(li_obj)
-
-plt.plot(numpy.mean(li_obj_all,axis=0),lw=5)
-print(numpy.mean(li_obj_all,axis=0))
-plt.ylabel('EM objective')
-plt.xlabel('EM iterations')
-plt.show()
-plt.close()
+			plot_everything(li_samples,li_labels,tm,phi,ax)
+			plt.pause(.1)
 
