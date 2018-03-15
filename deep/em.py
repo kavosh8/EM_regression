b068cbce0d0737b09181bcffbf8dfc81015f297f
Kavosh Asadi <kavosh@Kavoshs-MacBook-Air.local>
2018-03-15 20:44:28 +0330

    beginning of work in iran

--- deep/em.py
+++ deep/em.py
@@ -1,13 +1,15 @@
 import numpy
-
+import sys
 
 class em_learner:
 	"""docstring for ClassName"""
 	def __init__(self, params):
 		self.num_iterations=params['num_iterations']
 		self.gaussian_variance=params['gaussian_variance']
+		self.N=params['num_models']
+		self.learned_priors=(self.N)*[1.0/self.N]
 
-	def compute_posterior(self,tm,phi,y):
+	def compute_posterior(self,tm,phi,y,iteration_number):
 		o_li=tm.predict(phi)
 		#print("o_li",o_li)
 		y_arr = numpy.squeeze(numpy.asarray(y)).reshape(len(y),1)
@@ -18,27 +20,45 @@ class em_learner:
 		#print("o_li_diff",o_li_diff)
 		#o_li_diff=-numpy.multiply(o_li_diff,o_li_diff)/gaussian_variance
 		p_arr=numpy.zeros_like(o_li_diff)
-		for i in range(o_li_diff.shape[0]):
-				
+		obj=0
+		for i in range(o_li_diff.shape[0]):	
 				#print(i,"o_li_diff",o_li_diff[i,:])
 				mult=-numpy.multiply(o_li_diff[i,:],o_li_diff[i,:])/self.gaussian_variance
 				#print(i,"mult",mult)
+				#sys.exit(1)
 				clipped=numpy.clip(mult,a_min=-600, a_max=600)
 				#print(i,"clipped",clipped)
-				sum_val=numpy.sum(numpy.exp(clipped))
+				exped=numpy.exp(clipped)
+				top=numpy.multiply(exped,numpy.array(self.learned_priors))
+				down=numpy.sum(top)
 				
 				#print(numpy.exp(shifted)/sum_val)
-				p_arr[i,:]=numpy.exp(clipped)/sum_val
+				p_arr[i,:]=top/down
+				temp=numpy.multiply(p_arr[i,:],mult)+numpy.multiply(p_arr[i,:],numpy.log(self.learned_priors))
+				#print(numpy.sum(temp))
+				obj=obj+numpy.sum(temp)
 				#print(i,p_arr[i,:])
+				#sys.exit(1)
 		p_arr=numpy.transpose(p_arr)
 		#print(p_arr)
 		p_li=[]
 		for i in range(len(p_arr)):
 			p_li.append(numpy.clip(p_arr[i,:],a_min=1e-20, a_max=1))
 		#print("p_li",p_li)
+		print("obj",obj)
 		return p_li
+	def compute_learned_prior(self,w_li):
+		for n in range(self.N):
+			self.learned_priors[n]=numpy.mean(w_li[n])
+		print("priors:",self.learned_priors)
+		#print(self.learned_priors)
+		#sys.exit(1)
 
 	def e_step_m_step(self,tm,phi,y,iteration):
-		w_li=self.compute_posterior(tm,phi,y)#E step
+		w_li=self.compute_posterior(tm,phi,y,iteration)#E step
+		#print(w_li)
+		#sys.exit(1)
 		if iteration>0:
 			tm.regression(phi,y,w_li)#M step
+			self.compute_learned_prior(w_li)
+			#sys.exit(1)
--- deep/main.py
+++ deep/main.py
@@ -6,21 +6,36 @@ import transition_model
 import math
 import em
 
-def create_train_data(num_samples,num_lines):
+def create_train_data(li_num_samples,num_lines):
 	li_samples=[]
 	li_labels=[]
 	for l in range(num_lines):
-		for n in range(num_samples):
+		for n in range(li_num_samples[l]):
 			sample=numpy.random.uniform(-2,2)
 			if l==0:
+				li_samples.append(sample)
 				label=numpy.tanh(sample)+3#+numpy.random.normal(loc=0.0, scale=0.0,size=1)
+				li_labels.append(label)
+					
 			elif l==1:
 				label=sample*sample#+numpy.random.normal(loc=0.0, scale=0.0,size=1)
+				li_samples.append(sample)
+				li_labels.append(label)
+			elif l==2:
+				label=numpy.sin(sample)-5#+numpy.random.normal(loc=0.0, scale=0.0,size=1)
+				li_samples.append(sample)
+				li_labels.append(label)
+			elif l==3:
+				label=numpy.sin(sample)-3#+numpy.random.normal(loc=0.0, scale=0.0,size=1)
+				li_samples.append(sample)
+				li_labels.append(label)
+			elif l==4:
+				label=numpy.sin(sample)*numpy.sin(sample)#+numpy.random.normal(loc=0.0, scale=0.0,size=1)
+				li_samples.append(sample)
+				li_labels.append(label)
 			else:
 				print('not implemented yet ... aborting')
 				sys.exit(1)
-			li_samples.append(sample)
-			li_labels.append(label)
 			#print(sample,label)
 	return li_samples,li_labels
 def create_matrices(li_samples,li_labels):
@@ -38,36 +53,37 @@ def plot_everything(li_samples,li_labels,tm,phi,ax):
 	ax.clear()
 	ax.plot(li_samples,li_labels,'o')
 	y_li=tm.predict(phi)
-	for y in y_li:
-		ax.plot(li_samples,y,'o',lw=2)
+	for index,y in enumerate(y_li):
+		ax.plot(li_samples,y,'o',lw=2,label=index)
+	plt.legend()
 
 
 num_experiments=1
-num_samples=50
-num_lines=2
+li_num_samples=[20,20,20,20,20]
+num_lines=len(li_num_samples)
 plot=True
-
-em_params={}
-em_params['num_iterations']=200
-em_params['gaussian_variance']=.005
 
 
 model_params={}
-model_params['lipschitz_constant']=0.1
+model_params['lipschitz_constant']=.2
 model_params['num_hidden_layers']=2
 model_params['hidden_layer_nodes']=32
 model_params['activation_fn']='relu'
 model_params['learning_rate']=0.0001
 model_params['observation_size']=1
-model_params['num_models']=2
+model_params['num_models']=num_lines
 model_params['num_epochs']=100
 
+em_params={}
+em_params['num_iterations']=200
+em_params['gaussian_variance']=.05
+em_params['num_models']=model_params['num_models']
 
 fig = plt.figure()
 ax = fig.add_subplot(1,1,1)
 li_obj_all=[]
 for experiment in range(num_experiments):
-	li_samples,li_labels=create_train_data(num_samples,num_lines)
+	li_samples,li_labels=create_train_data(li_num_samples,num_lines)
 	phi,y=create_matrices(li_samples,li_labels)
 	tm=transition_model.neural_transition_model(model_params)
 	em_object=em.em_learner(em_params)
