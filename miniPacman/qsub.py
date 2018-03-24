4f757669e7991ea41d62b5720fc842b47d7a0569
Kavosh Asadi <kavosh@Kavoshs-MacBook-Air.local>
2018-03-24 13:44:09 +0430

    change qsub for gaussian variance choices

--- miniPacman/qsub.py
+++ miniPacman/qsub.py
@@ -17,7 +17,7 @@ python main.py {} {} {} {} {}
 for run_number in range(5):
 	for num_samples in [1000,2000]:
 		for learning_rate in [0.005,0.001,0.0005]:
-			for gaussian_variance in [0.2,0.1,0.05]:
+			for gaussian_variance in [0.5,0.1,0.05]:
 				for num_hidden_layers in [0,1,2]:
 					fname="log/w_loss-"+str(run_number)+"-"+str(num_samples)+"-"+str(learning_rate)+"-"+str(gaussian_variance)+"-"+str(num_hidden_layers)+".txt"
 					if os.path.isfile(fname)==False:
